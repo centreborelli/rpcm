07560868ad28515d182cfd65df55ca4b027e1127
Carlo de Franchis <carlodef@gmail.com>
2020-09-23 20:55:11 +0200

    update version number to 1.4.7

--- rpcm/__about__.py
+++ rpcm/__about__.py
@@ -1,12 +1,6 @@
-VERSION = (
-    1,
-    4,
-    6,
-)
-
 __title__ = "rpcm"
 __description__ = "Rational Polynomial Camera Model for optical satellite images."
 __url__ = 'https://github.com/cmla/rpcm'
-__version__ = ".".join(map(str, VERSION))
+__version__ = "1.4.7"
 __author__ = "Carlo de Franchis"
 __author_email__ = 'carlo.de-franchis@ens-cachan.fr'
